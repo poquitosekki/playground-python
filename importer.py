975c9b4f8381702ca03be896d34d81943c96b0e7
poquitosekki <aymanbelhaj01@gmail.com>
2020-01-02 11:55:31 +0100

    standard library

--- importer.py
+++ importer.py
@@ -2,7 +2,12 @@
 # form modules import * (everything)
 from modules import find_index, test
 import sys
-
+# The standard Library
+import random
+import math
+import datetime
+import calendar
+import os
 
 courses = ["Python", "Turtles", "Pandas", "Tkinter", "Numpy"]
 
@@ -22,3 +27,23 @@ print(sys.path)
 # Set in python path : type in terminal
 # nano ~/.bash_profile
 # add : export PYTHONPATH="the path here"
+
+# The random module
+random_course = random.choice(courses)
+print(random_course)
+
+# The math module
+rads = math.radians(90)
+print(math.sin(rads))
+
+# The datetime, calendar module
+today = datetime.date.today()
+print(today)
+
+print(calendar.isleap(2020))
+
+# The OS module
+# get the current working directory
+print(os.getcwd())
+# print where os.py lives in your computer
+print(os.__file__)
