a968711f71bfc1878d7d9cb08c0f275954e9b505
poquitosekki <aymanbelhaj01@gmail.com>
2020-01-04 20:28:31 +0100

    Add the comprehesions py

---
+++ comprehensions.py
@@ -0,0 +1,46 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_list = []
+
+# We want n for each n in nums assign it to my_list
+# Traditional method
+# for n in nums:
+#     my_list.append(n)
+# print(my_list)
+
+# List comprehension
+# We want n for each n in nums
+# my_list = [n for n in nums]
+# print(my_list)
+
+############################################
+
+# We want n*n for each n in nums
+# Traditional method
+# my_list = []
+# for n in nums:
+#     my_list.append(n*n)
+# print(my_list)
+
+# List comprehension
+# my_list = [n*n for n in nums]
+# print(my_list)
+
+# Map and lambdas method
+# my_list = list(map(lambda n: n*n, nums))
+# print(my_list)
+
+############################################
+
+# We want n for each n in nums but if n is even
+# Traditional method
+# my_list = []
+# for n in nums:
+#     if n % 2 == 0:
+#         my_list.append(n)
+# print(my_list)
+
+# list comprehension method
+# my_list = [n for n in nums if n % 2 == 0]
+# print(my_list)
+
+# Filter and lambdas method
